b5e06f77be99c8866085c1cf7a264486eb77f144
Tumworobere <annaht.annah@gmail.com>
2023-12-14 03:14:21 +0400

    Built a simple loop that print numbers to the consolefrom 0 to the length of an array

---
+++ printnum.js
@@ -0,0 +1,5 @@
+//Build a simple loop, that print numbers to the console. from 0 to the length of an array.
+const num = [0,1,2,3,4,5];
+for (let i = 0; i < num.length; i++) {
+    console.log(num[i]);
+}
