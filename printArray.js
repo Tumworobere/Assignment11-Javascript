4d987ac863b891916c8decf7095d0c9f8dfe106a
Tumworobere <annaht.annah@gmail.com>
2023-12-14 03:50:37 +0400

    Added a comment

--- printArray.js
+++ printArray.js
@@ -1,3 +1,4 @@
+//Build a simple loop, inside a function, that takes an array and prints the value of the array to the console
 function numArray() {
     const arr=[ "Hannah","Tannah","Samara","Amara"];
     for (let i = 0; i < arr.length; i++) {
