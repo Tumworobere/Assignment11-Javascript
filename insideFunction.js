5d5dd78c144752b18bc5b155734bd8e4dd06b2ea
Tumworobere <annaht.annah@gmail.com>
2023-12-14 04:06:32 +0400

    Built a function to display a text inside a function block

---
+++ insideFunction.js
@@ -0,0 +1,8 @@
+//Build a simple function that displays the following.
+//console.log('THis is a text inside a function block'
+function displayTextInFunctionBlock() {
+    const textInside= ('This is text inside a function block');
+    return textInside;
+}
+    console.log(displayTextInFunctionBlock());
+
