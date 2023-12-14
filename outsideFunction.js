b63ca1b2fd6e7cb9245f6ea95e6ea3fe25ce65a2
Tumworobere <annaht.annah@gmail.com>
2023-12-14 04:19:07 +0400

    Buitlt a function to display text outside the block

---
+++ outsideFunction.js
@@ -0,0 +1,10 @@
+//Display a simple message outside a function that says,
+//This is a text outside a function block
+console.log('This is text outside a function block');
+
+function displayTextOutFunctionBlock() {
+    const textOutside=('This is text inside a function block');
+    return textOutside;
+}
+console.log('This is text outside a function block');
+
